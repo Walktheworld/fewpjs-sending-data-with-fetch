04a1a79a437cbefb5b6ce5592bddbb68511e326b
Joe <Walktheworld@me.com>
2021-05-12 11:33:50 -0600

    commit tree a7696a13d5915ef7cb92ce65b5a3b1941ebfad50

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+let formData={
+    name:``,
+    email:``
+}
+
+function submitData(){
+    let configObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+      
+    
+    return fetch("http://localhost:3000/users", configObject)
+        .then(function(response) {
+         return response.json();
+         })
+        .then(function(object) {
+            console.log(object);
+        })
+        .catch(function(error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+        }); 
+}
